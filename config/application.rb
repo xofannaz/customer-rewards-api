37f96cc944820d1488dbcf800ba2cfeeb304d172
xofannaz <xofanna.guimaraes@gmail.com>
2025-02-20 06:48:31 -0300

    feat: points transaction endpoints

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,12 @@ module CustomerRewardsApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.smile = config_for(:smile)
+
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :options]
+      end
+    end
   end
 end
